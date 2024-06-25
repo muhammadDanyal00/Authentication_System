c97361e67f6018940205e1a7ffa344ea19604c48
Muhammad Danyal <149951268+muhammadDanyal00@users.noreply.github.com>
2024-06-25 23:27:31 +0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // connection btw frontend and backend
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api"; // backend URL
+const API_BASE_URL = "http://localhost:****/abc"; // backend URL ( update with your own )
 
 const api = axios.create({
   baseURL: API_BASE_URL,
